430fd1ded643986c15e7284a4a4d90ccccd43964
Basant Kumar <119030323+basantkumarr@users.noreply.github.com>
2024-04-30 21:55:56 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(cors({
 }));
 
 // Database connection
-mongoose.connect("mongodb+srv://basantkumarweb:753dzH2WQKLGJKeC@db-apple.zk1gplu.mongodb.net/?retryWrites=true&w=majority&appName=db-apple/user", {
+mongoose.connect("mongodb+srv://basantkumarweb:753dzH2WQKLGJKeC@db-apple.zk1gplu.mongodb.net/?retryWrites=true&w=majority&appName=db-apple/test", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true
