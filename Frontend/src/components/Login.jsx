f95beb895d45f424c9375ed7249e90c560cadc33
Basant Kumar <119030323+basantkumarr@users.noreply.github.com>
2024-04-30 14:44:59 +0530

    Update Login.jsx

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
     e.preventDefault();
 
     axios
-.post("http://localhost:3001/login", {        email,
+.post("apple-academy.vercel.app/login", {        email,
         password,
       })
       .then((result) => {
