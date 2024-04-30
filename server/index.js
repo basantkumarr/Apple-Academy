33d5b8eddd976dcc7f5e6beff3f738bcb962def4
Basant Kumar <119030323+basantkumarr@users.noreply.github.com>
2024-04-30 22:07:16 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-origin: process.env.CLIENT_URL,
+origin:"https://apple-classes.vercel.app",
   optionsSuccessStatus: 200,  
   methods: ["GET,HEAD,PUT,PATCH,POST,DELETE"],
   credentials: true,
